e5b64bbc6c1844f50220e07b4afe6b2d68f73ce5
Nick Firme <nickfirme@live.com>
2022-02-05 16:23:21 -0800

    Revert "Set up expo-dev-client and Firebase"
    This reverts commit e0015ee75506f05ffbae28b5c5a11d946f95173b.
    
    Undo Firebase and expo-dev-client setup

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my React Native app!</Text>
-      <Text>Testing</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
