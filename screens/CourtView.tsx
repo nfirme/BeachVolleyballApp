3aa0818ee03487f5254d1817d027ed548d774d40
Nick Firme <nickfirme@live.com>
2022-02-19 15:44:46 -0800

    Basic app set up

--- screens/CourtView.tsx
+++ screens/CourtView.tsx
@@ -1,12 +1,11 @@
-import { View, StyleSheet } from 'react-native'
-import { CourtGrid, RecordInputs } from '../components';
-import { screenStyles } from '../styles/screens';
+import { View } from 'react-native'
+import { CourtGrid } from '../components';
+import { screenStyles } from '../styles';
 
 export default function CourtView() {
   return (
     <View style={screenStyles.screen}>
       <CourtGrid />
-      <RecordInputs />
     </View>
   );
 }
